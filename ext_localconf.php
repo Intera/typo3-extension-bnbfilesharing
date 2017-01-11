ddf19fd6c4b4310a70533843a11ef7f07132ef87
Alexander Stehlik <alexander.stehlik@gmail.com>
2017-01-11 16:17:32 +0100

    [TASK] Migrate new content wizard config to PageTSconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,4 +18,11 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	)
 );
 
-?>
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE: EXT:bnbfilesharing/Configuration/PageTSconfig/NewContentElementWizard.pagets">
+
+    // Woraround for TYPO3 6.2 which does not support EXT: syntax for icons.
+    mod.wizards.newContentElement.wizardItems.special.elements.bnbfilesharing_filesharing.icon ='
+    . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY)
+    . '/Resources/Public/Icons/NewContentElementWizard.jpg'
+);
