bfbddac86d30a09575f0bad026177e8d067c34e8
Alexander Stehlik <alexander.stehlik@gmail.com>
2017-01-11 22:47:23 +0100

    [TASK] Adjust / update unit tests

--- Tests/Unit/Domain/Model/FilesTest.php
+++ Tests/Unit/Domain/Model/FilesTest.php
@@ -1,167 +1,155 @@
 <?php
+namespace Tx\Bnbfilesharing\Tests\Unit\Domain\Model;
 
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2011 
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2011
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Tx\Bnbfilesharing\Domain\Model\File;
+use Tx\Bnbfilesharing\Domain\Model\Folder;
+use TYPO3\CMS\Core\Tests\BaseTestCase;
+use TYPO3\CMS\Extbase\Domain\Model\FrontendUser;
 
 /**
- * Test case for class Tx_Bnbfilesharing_Domain_Model_Files.
+ * Test case for class tx_bnbfilesharing_domain_model_file.
  *
- * @version $Id$
  * @copyright Copyright belongs to the respective authors
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- *
- * @package TYPO3
- * @subpackage bnb filesharing
- *
  */
-class Tx_Bnbfilesharing_Domain_Model_FilesTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
-	/**
-	 * @var Tx_Bnbfilesharing_Domain_Model_Files
-	 */
-	protected $fixture;
+class FileTest extends BaseTestCase
+{
+    /**
+     * @var File
+     */
+    protected $fixture;
 
-	public function setUp() {
-		$this->fixture = new Tx_Bnbfilesharing_Domain_Model_Files();
-	}
+    public function setUp()
+    {
+        $this->fixture = new File();
+    }
 
-	public function tearDown() {
-		unset($this->fixture);
-	}
-	
-	
-	/**
-	 * @test
-	 */
-	public function getFileReturnsInitialValueForString() { }
+    public function tearDown()
+    {
+        unset($this->fixture);
+    }
 
-	/**
-	 * @test
-	 */
-	public function setFileForStringSetsFile() { 
-		$this->fixture->setFile('Conceived at T3CON10');
+    /**
+     * @test
+     */
+    public function addFolderToObjectStorageHoldingFolder()
+    {
+        $folder = new Folder();
+        $this->fixture->setFolder($folder);
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getFile()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getBeschriftungReturnsInitialValueForString() { }
+        $this->assertEquals(
+            $folder,
+            $this->fixture->getFolder()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setBeschriftungForStringSetsBeschriftung() { 
-		$this->fixture->setBeschriftung('Conceived at T3CON10');
+    /**
+     * @test
+     */
+    public function getBeschriftungReturnsInitialValueForString()
+    {
+    }
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getBeschriftung()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getFeuseridReturnsInitialValueForInteger() { 
-		$this->assertSame(
-			0,
-			$this->fixture->getFeuserid()
-		);
-	}
+    /**
+     * @test
+     */
+    public function getFeuseridReturnsInitialValueForInteger()
+    {
+        $this->assertSame(
+            null,
+            $this->fixture->getFeuser()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setFeuseridForIntegerSetsFeuserid() { 
-		$this->fixture->setFeuserid(12);
+    /**
+     * @test
+     */
+    public function getFileReturnsInitialValueForString()
+    {
+    }
 
-		$this->assertSame(
-			12,
-			$this->fixture->getFeuserid()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getFolderReturnsInitialValueForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_Folder() { 
-		$newObjectStorage = new Tx_Extbase_Persistence_ObjectStorage();
-		$this->assertEquals(
-			$newObjectStorage,
-			$this->fixture->getFolder()
-		);
-	}
+    /**
+     * @test
+     */
+    public function getFolderReturnsInitialValueForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_Folder()
+    {
+        $this->assertEquals(
+            null,
+            $this->fixture->getFolder()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setFolderForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_FolderSetsFolder() { 
-		$folder = new Tx_Bnbfilesharing_Domain_Model_Folder();
-		$objectStorageHoldingExactlyOneFolder = new Tx_Extbase_Persistence_ObjectStorage();
-		$objectStorageHoldingExactlyOneFolder->attach($folder);
-		$this->fixture->setFolder($objectStorageHoldingExactlyOneFolder);
+    /**
+     * @test
+     * @expectedException \PHPUnit_Framework_Error
+     */
+    public function removeFolderFromObjectStorageHoldingFolder()
+    {
+        $this->fixture->setFolder(null);
+    }
 
-		$this->assertSame(
-			$objectStorageHoldingExactlyOneFolder,
-			$this->fixture->getFolder()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function addFolderToObjectStorageHoldingFolder() {
-		$folder = new Tx_Bnbfilesharing_Domain_Model_Folder();
-		$objectStorageHoldingExactlyOneFolder = new Tx_Extbase_Persistence_ObjectStorage();
-		$objectStorageHoldingExactlyOneFolder->attach($folder);
-		$this->fixture->addFolder($folder);
+    /**
+     * @test
+     */
+    public function setBeschriftungForStringSetsBeschriftung()
+    {
+        $this->fixture->setLabel('Conceived at T3CON10');
 
-		$this->assertEquals(
-			$objectStorageHoldingExactlyOneFolder,
-			$this->fixture->getFolder()
-		);
-	}
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->fixture->getLabel()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function removeFolderFromObjectStorageHoldingFolder() {
-		$folder = new Tx_Bnbfilesharing_Domain_Model_Folder();
-		$localObjectStorage = new Tx_Extbase_Persistence_ObjectStorage();
-		$localObjectStorage->attach($folder);
-		$localObjectStorage->detach($folder);
-		$this->fixture->addFolder($folder);
-		$this->fixture->removeFolder($folder);
+    /**
+     * @test
+     */
+    public function setFeuseridForIntegerSetsFeuserid()
+    {
+        $frontendUser = new FrontendUser();
+        $frontendUser->setUsername('testuser');
 
-		$this->assertEquals(
-			$localObjectStorage,
-			$this->fixture->getFolder()
-		);
-	}
-	
+        $this->fixture->setFeuser($frontendUser);
+
+        $this->assertSame(
+            $frontendUser,
+            $this->fixture->getFeuser()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setFileForStringSetsFile()
+    {
+        $this->fixture->setFile('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->fixture->getFile()
+        );
+    }
 }
-?>
--- Tests/Unit/Domain/Model/FolderTest.php
+++ Tests/Unit/Domain/Model/FolderTest.php
@@ -1,186 +1,195 @@
 <?php
+namespace Tx\Bnbfilesharing\Tests\Unit\Domain\Model;
 
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2011 
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2011
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Tx\Bnbfilesharing\Domain\Model\File;
+use Tx\Bnbfilesharing\Domain\Model\Folder;
+use TYPO3\CMS\Core\Tests\BaseTestCase;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 /**
  * Test case for class Tx_Bnbfilesharing_Domain_Model_Folder.
  *
- * @version $Id$
  * @copyright Copyright belongs to the respective authors
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- *
- * @package TYPO3
- * @subpackage bnb filesharing
- *
  */
-class Tx_Bnbfilesharing_Domain_Model_FolderTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
-	/**
-	 * @var Tx_Bnbfilesharing_Domain_Model_Folder
-	 */
-	protected $fixture;
+class FolderTest extends BaseTestCase
+{
+    /**
+     * @var Folder
+     */
+    protected $fixture;
 
-	public function setUp() {
-		$this->fixture = new Tx_Bnbfilesharing_Domain_Model_Folder();
-	}
+    public function setUp()
+    {
+        $this->fixture = new Folder();
+    }
 
-	public function tearDown() {
-		unset($this->fixture);
-	}
-	
-	
-	/**
-	 * @test
-	 */
-	public function getNameReturnsInitialValueForString() { }
+    public function tearDown()
+    {
+        unset($this->fixture);
+    }
 
-	/**
-	 * @test
-	 */
-	public function setNameForStringSetsName() { 
-		$this->fixture->setName('Conceived at T3CON10');
+    /**
+     * @test
+     */
+    public function addChildToObjectStorageHoldingChildren()
+    {
+        $child = new Folder();
+        $this->fixture->addChild($child);
 
-		$this->assertSame(
-			'Conceived at T3CON10',
-			$this->fixture->getName()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getChildrenReturnsInitialValueForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_Folder() { 
-		$newObjectStorage = new Tx_Extbase_Persistence_ObjectStorage();
-		$this->assertEquals(
-			$newObjectStorage,
-			$this->fixture->getChildren()
-		);
-	}
+        $objectStorageHoldingExactlyOneChild = new ObjectStorage();
+        $objectStorageHoldingExactlyOneChild->attach($child);
 
-	/**
-	 * @test
-	 */
-	public function setChildrenForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_FolderSetsChildren() { 
-		$child = new Tx_Bnbfilesharing_Domain_Model_Folder();
-		$objectStorageHoldingExactlyOneChildren = new Tx_Extbase_Persistence_ObjectStorage();
-		$objectStorageHoldingExactlyOneChildren->attach($child);
-		$this->fixture->setChildren($objectStorageHoldingExactlyOneChildren);
+        $this->assertEquals(
+            $objectStorageHoldingExactlyOneChild,
+            $this->fixture->getChildren()
+        );
+    }
 
-		$this->assertSame(
-			$objectStorageHoldingExactlyOneChildren,
-			$this->fixture->getChildren()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function addChildToObjectStorageHoldingChildren() {
-		$child = new Tx_Bnbfilesharing_Domain_Model_Folder();
-		$objectStorageHoldingExactlyOneChild = new Tx_Extbase_Persistence_ObjectStorage();
-		$objectStorageHoldingExactlyOneChild->attach($child);
-		$this->fixture->addChild($child);
+    /**
+     * @test
+     */
+    public function addFileToObjectStorageHoldingFiles()
+    {
+        $file = new File();
+        $this->fixture->addFile($file);
 
-		$this->assertEquals(
-			$objectStorageHoldingExactlyOneChild,
-			$this->fixture->getChildren()
-		);
-	}
+        $objectStorageHoldingExactlyOneFile = new ObjectStorage();
+        $objectStorageHoldingExactlyOneFile->attach($file);
 
-	/**
-	 * @test
-	 */
-	public function removeChildFromObjectStorageHoldingChildren() {
-		$child = new Tx_Bnbfilesharing_Domain_Model_Folder();
-		$localObjectStorage = new Tx_Extbase_Persistence_ObjectStorage();
-		$localObjectStorage->attach($child);
-		$localObjectStorage->detach($child);
-		$this->fixture->addChild($child);
-		$this->fixture->removeChild($child);
+        $this->assertEquals(
+            $objectStorageHoldingExactlyOneFile,
+            $this->fixture->getFiles()
+        );
+    }
 
-		$this->assertEquals(
-			$localObjectStorage,
-			$this->fixture->getChildren()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function getFilesReturnsInitialValueForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_Files() { 
-		$newObjectStorage = new Tx_Extbase_Persistence_ObjectStorage();
-		$this->assertEquals(
-			$newObjectStorage,
-			$this->fixture->getFiles()
-		);
-	}
+    /**
+     * @test
+     */
+    public function getChildrenReturnsInitialValueForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_Folder()
+    {
+        $newObjectStorage = new ObjectStorage();
+        $this->assertEquals(
+            $newObjectStorage,
+            $this->fixture->getChildren()
+        );
+    }
 
-	/**
-	 * @test
-	 */
-	public function setFilesForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_FilesSetsFiles() { 
-		$file = new Tx_Bnbfilesharing_Domain_Model_Files();
-		$objectStorageHoldingExactlyOneFiles = new Tx_Extbase_Persistence_ObjectStorage();
-		$objectStorageHoldingExactlyOneFiles->attach($file);
-		$this->fixture->setFiles($objectStorageHoldingExactlyOneFiles);
+    /**
+     * @test
+     */
+    public function getFilesReturnsInitialValueForObjectStorageContainingtx_bnbfilesharing_domain_model_file()
+    {
+        $newObjectStorage = new ObjectStorage();
+        $this->assertEquals(
+            $newObjectStorage,
+            $this->fixture->getFiles()
+        );
+    }
 
-		$this->assertSame(
-			$objectStorageHoldingExactlyOneFiles,
-			$this->fixture->getFiles()
-		);
-	}
-	
-	/**
-	 * @test
-	 */
-	public function addFileToObjectStorageHoldingFiles() {
-		$file = new Tx_Bnbfilesharing_Domain_Model_Files();
-		$objectStorageHoldingExactlyOneFile = new Tx_Extbase_Persistence_ObjectStorage();
-		$objectStorageHoldingExactlyOneFile->attach($file);
-		$this->fixture->addFile($file);
+    /**
+     * @test
+     */
+    public function removeChildFromObjectStorageHoldingChildren()
+    {
+        $child = new Folder();
+        $this->fixture->addChild($child);
+        $this->fixture->removeChild($child);
 
-		$this->assertEquals(
-			$objectStorageHoldingExactlyOneFile,
-			$this->fixture->getFiles()
-		);
-	}
+        $localObjectStorage = new ObjectStorage();
+        $localObjectStorage->attach($child);
+        $localObjectStorage->detach($child);
 
-	/**
-	 * @test
-	 */
-	public function removeFileFromObjectStorageHoldingFiles() {
-		$file = new Tx_Bnbfilesharing_Domain_Model_Files();
-		$localObjectStorage = new Tx_Extbase_Persistence_ObjectStorage();
-		$localObjectStorage->attach($file);
-		$localObjectStorage->detach($file);
-		$this->fixture->addFile($file);
-		$this->fixture->removeFile($file);
+        $this->assertEquals(
+            $localObjectStorage,
+            $this->fixture->getChildren()
+        );
+    }
 
-		$this->assertEquals(
-			$localObjectStorage,
-			$this->fixture->getFiles()
-		);
-	}
-	
+    /**
+     * @test
+     */
+    public function removeFileFromObjectStorageHoldingFiles()
+    {
+        $file = new File();
+        $this->fixture->addFile($file);
+        $this->fixture->removeFile($file);
+
+        $localObjectStorage = new ObjectStorage();
+        $localObjectStorage->attach($file);
+        $localObjectStorage->detach($file);
+
+        $this->assertEquals(
+            $localObjectStorage,
+            $this->fixture->getFiles()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setChildrenForObjectStorageContainingTx_Bnbfilesharing_Domain_Model_FolderSetsChildren()
+    {
+        $child = new Folder();
+        $objectStorageHoldingExactlyOneChildren = new ObjectStorage();
+        $objectStorageHoldingExactlyOneChildren->attach($child);
+        $this->fixture->setChildren($objectStorageHoldingExactlyOneChildren);
+
+        $this->assertSame(
+            $objectStorageHoldingExactlyOneChildren,
+            $this->fixture->getChildren()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setFilesForObjectStorageContainingtx_bnbfilesharing_domain_model_fileSetsFiles()
+    {
+        $file = new File();
+        $objectStorageHoldingExactlyOneFiles = new ObjectStorage();
+        $objectStorageHoldingExactlyOneFiles->attach($file);
+        $this->fixture->setFiles($objectStorageHoldingExactlyOneFiles);
+
+        $this->assertSame(
+            $objectStorageHoldingExactlyOneFiles,
+            $this->fixture->getFiles()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setNameForStringSetsName()
+    {
+        $this->fixture->setName('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->fixture->getName()
+        );
+    }
 }
-?>
